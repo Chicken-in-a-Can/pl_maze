223f8d40575a7aab083855cb895410fe7ab6930f
Ben Stidham <ben.stidham1@gmail.com>
2025-05-11 18:07:04 -0500

    most of the code is here, now just debugging

--- devlog.md
+++ devlog.md
@@ -1,3 +1,7 @@
 # 5/11/2025 3:00-4:30
 
 Create trace function, find/check at index function, and a couple others for debugging sake
+
+# 5/11/2025 5:00-6:00
+
+Code mostly written, debugging
--- maze.pl
+++ maze.pl
@@ -1,4 +1,5 @@
-trace(Map, [], [X, Y]) :- check_loc(Map, X, Y, "e").
+% Trace the map
+trace(Map, [], [X, Y]) :- check_loc(Map, X, Y, e).
 
 trace(Map, [up|InsT], [X, Y]) :-
     check_loc(Map, X, Y-1, f),
@@ -22,12 +23,40 @@ check_loc(Map, X, Y, Ele) :-
     nth0(Y, Map, Row),
     nth0(X, Row, Ele).
 
+% Display map as series of points
 disp_map(Map) :-
     check_loc(Map, X, Y, Ele),
     write("("), write(X), write(","), write(Y),
     write(") = "), write(Ele), nl, false.
 
-find_exit([], _) :- false.
-%find_exit(Map, Actions).
+% Right
+move([X, Y], [Nx, Y], right) :- Nx is X + 1.
+% Left
+move([X, Y], [Nx, Y], left) :- Nx is X - 1.
+% Down
+move([X, Y], [X, Ny], down) :- Ny is Y + 1.
+% Up
+move([X, Y], [X, Ny], up) :- Ny is Y - 1.
 
+% Check if given element is a member of the list
+memberCheck([_|T], H) :- memberCheck(T, H).
+memberCheck([H|_], H).
 
+% Recursively iterate through the map, finding the correct path
+iter_map(_, [EndX, EndY], [EndX, EndY], _, _).
+
+iter_map(Map, [X, Y], [EndX, EndY], Visited, Actions) :-
+    move([X, Y], [Nx, Ny], Dir),
+    (check_loc(Map, Nx, Ny, f); check_loc(Map, Nx, Ny, e)),
+    memberCheck(Visited, [Nx, Ny]),
+    write("Iterating"), nl,
+    iter_map(Map, [Nx, Ny], [EndX, EndY], [Visited|[Nx, Ny]], [Actions|Dir]).
+
+% Main function to call
+%find_exit([], _) :- false.
+find_exit(Map, Actions) :-
+    check_loc(Map, X, Y, s),
+    check_loc(Map, EndX, EndY, e),
+    write("Initial position at: ("), write(X), write(","), write(Y), write(")"), nl,
+    write("Final position at: ("), write(EndX), write(","), write(EndY), write(")"), nl,
+    iter_map(Map, [X, Y], [EndX, EndY], [], Actions).
